be60ef7861171fb17ac1d787c07aa899168ed207
jynti <33417806+jynti@users.noreply.github.com>
2018-01-10 13:11:31 +0530

    Update restore_commands.sql

--- restore_commands.sql
+++ restore_commands.sql
@@ -1,7 +1,14 @@
+--creating backup in command line
+mysqldump -u root -p DVDRentals > ~/database/backup_exercise4_100118.sql
+----------------------------------
 mysql> CREATE DATABASE restored;
 Query OK, 1 row affected (0.00 sec)
 
 mysql> USE restored;
 Database changed
+
+--restoring backup using mysql client utility in interactive mode
 mysql> source ~/database/backup_exercise4_100118.sql
 
+--restoring backup using command line
+mysql -u root -p restored < ~/database/backup_exercise4_100118.sql
